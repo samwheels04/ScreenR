a965623ba68e316a8d4b27631ca5555ba7d005f8
Johnny Wilkens <jwilkens@wesleyan.edu>
2023-11-09 16:05:47 -0500

    added master screen function

---
+++ screenmaster.R
@@ -0,0 +1,11 @@
+## screen master function
+
+
+screen <- function(formula, data, threshold){
+  
+  # stop msgs
+  # missing data
+  # constant variables(nonZeroVariance)
+  # statistical tests
+  
+}
