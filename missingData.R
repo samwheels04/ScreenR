7b5e685bafcda4bae5b6d8343d282aaef7e9009c
Sam Wheeler <swheeler02@wesleyan.edu>
2023-11-14 15:45:18 -0500

    missingData function

---
+++ missingData.R
@@ -0,0 +1,42 @@
+# install and/or load package
+pacman::p_load(naniar)
+
+# returns vectors of column names that have less than 40% of the data missing
+missing.data <- function(data, var.names, threshold = 0.4) {
+    darwinism <- c()
+    for (var in var.names) {
+        miss <- pct_miss(data[[var]])/100
+        if (miss < threshold) {
+            darwinism <- c(darwinism, var)
+        }
+    }
+    return(darwinism)
+}
+
+#example
+pct_miss(airquality[["Ozone"]])
+pct_miss(airquality[["Solar.R"]])
+
+missing.data(airquality, c("Ozone", "Solar.R"), threshold = 0.2)
+missing.data(airquality, c("Ozone", "Solar.R"), threshold = 0.3)
+missing.data(airquality, c("Ozone", "Solar.R"))
+
+
+
+
+
+#returns p value for cor between two quantitative variables
+test.QQ <- function(x, y) {
+    cor <- cor.test(x, y)
+    return(cor$p.value)
+}
+test.QQ(mtcars$mpg, mtcars$wt)
+InsectSprays
+#returns p value for cor between two categorical variables
+test.CC <- function(x, y) {
+
+}
+
+
+head(airquality)
+?anova()
